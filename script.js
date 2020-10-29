9f7978839a4aa279480ff74da2aff21d1d1a7de0
DragosR2020 <70966753+DragosR2020@users.noreply.github.com>
2020-10-29 16:00:35 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function bomb() {
+  document.body.style.background = "url('bomb.gif')";
+  document.body.style.backgroundRepeat = "no-repeat";
+  document.body.style.backgroundSize = "cover";
+  document.body.style.backgroundPosition = "center";
+  document.getElementById("button").style.display = "none";
+}
